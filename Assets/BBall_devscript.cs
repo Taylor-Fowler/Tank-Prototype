68a8cb8ba33a4f9262e12d117efe8a3cb87067b2
Max Bryans <maxsfw3@gmail.com>
2018-10-26 11:26:43 +0100

    Physics_Playground added

---
+++ Assets/BBall_devscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BBall_devscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 3f);
+        Rigidbody RB = GetComponent<Rigidbody>();
+        RB.velocity = (transform.forward * 15f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
